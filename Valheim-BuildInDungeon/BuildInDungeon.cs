df9d954e9e98511397ab9d975513d4ee2a27bc7b
Charlotte Lily-Llaitchee Vixen <CharlotteLilyFields48@gmail.com>
2021-02-28 23:40:40 +0000

    first real commit
    all done for v1.0.0

---
+++ Valheim-BuildInDungeon/BuildInDungeon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Valheim_BuildInDungeon
+{
+	[BepInPlugin(GUID, Name, Version)]
+	[BepInProcess("valheim.exe")]
+	[HarmonyPatch]
+	public class ChangePlacePiece : BaseUnityPlugin
+	{
+		private const string GUID = "valheim.lottievixen.build_in_dungeons";
+		private const string Name = "Build In Dungeons";
+		private const string Version = "1.0.0.0";
+		void Awake() 
+		{
+			var harmony = new Harmony("valheim.lottievixen.build_in_dungeons");
+			harmony.PatchAll();
+		}
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(Piece), "Awake")]
+		static void Placement_Patch(Piece __instance)
+		{
+			__instance.m_allowedInDungeons = true;
+		}
+		/* removed as it is unnessecary
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(Character), "InInterior")]
+		static void Placement_char(ref bool __result) {
+			__result = false;
+		}
+		*/
+	}
+}
---
+++ Valheim-BuildInDungeon/README.md
@@ -0,0 +1,6 @@
+# Build in Dungeons
+A mod for Valheim to allow building in dungeons
+
+# Build instructions
+add references to Harmony and BepInEx as well as UnityEngine and UnityEngine.CoreModule,
+as well as assembly_valheim publicized then build, zip the resulting directory name appropriately and you're done :D 
